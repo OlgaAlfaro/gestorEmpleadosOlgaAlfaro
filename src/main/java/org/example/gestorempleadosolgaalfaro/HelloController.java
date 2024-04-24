287f29802f181756de6979086672364a4ea3e6a9
OlgaAlfaro <oalfarolas1@educacion.navarra.es>
2024-04-24 19:13:44 +0200

    Relacionado fxml con el controlador, y comenzado el metodo para el boton Insertar

--- src/main/java/org/example/gestorempleadosolgaalfaro/HelloController.java
+++ src/main/java/org/example/gestorempleadosolgaalfaro/HelloController.java
@@ -1,14 +1,28 @@
 package org.example.gestorempleadosolgaalfaro;
 
+import Modelo.Trabajador;
 import javafx.fxml.FXML;
-import javafx.scene.control.Label;
+import javafx.scene.control.*;
 
 public class HelloController {
     @FXML
-    private Label welcomeText;
+    private TextField txtFieldNombre;
+    @FXML
+    private ComboBox<String> cmbBoxPuesto;
+    @FXML
+    private TextField txtFieldSalario;
+
 
     @FXML
     protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+        String nombre = txtFieldNombre.getText();
+        String puesto = cmbBoxPuesto.getValue();
+        Integer salario = Integer.valueOf(txtFieldSalario.getText());
+        Trabajador trabajador = new Trabajador(nombre, puesto, salario);
+        Alert alerta = new Alert(Alert.AlertType.INFORMATION);
+        alerta.setTitle("HECHO");
+        alerta.setHeaderText("Mensaje");
+        alerta.setContentText("Empleado " + nombre + " introducido en la base de datos satisfactoriamente.");
+        alerta.showAndWait();
     }
 }
