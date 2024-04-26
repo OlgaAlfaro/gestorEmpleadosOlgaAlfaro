1d50825c0a2532ab24f269ac4f1f475fd34da6a2
OlgaAlfaro <oalfarolas1@educacion.navarra.es>
2024-04-26 12:05:48 +0200

    Conseguido conectar con la base de datos e introducir un trabajador

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,6 +1,9 @@
 module org.example.gestorempleadosolgaalfaro {
     requires javafx.controls;
     requires javafx.fxml;
+    requires java.sql;
+    requires jdk.jdi;
+    requires jdk.jconsole;
 
 
     opens org.example.gestorempleadosolgaalfaro to javafx.fxml;
--- src/main/java/org/example/gestorempleadosolgaalfaro/HelloController.java
+++ src/main/java/org/example/gestorempleadosolgaalfaro/HelloController.java
@@ -4,6 +4,8 @@ import Modelo.Trabajador;
 import javafx.fxml.FXML;
 import javafx.scene.control.*;
 
+import java.sql.SQLException;
+
 public class HelloController {
     @FXML
     private TextField txtFieldNombre;
@@ -14,11 +16,12 @@ public class HelloController {
 
 
     @FXML
-    protected void onHelloButtonClick() {
+    protected void onHelloButtonClick() throws SQLException {
         String nombre = txtFieldNombre.getText();
         String puesto = cmbBoxPuesto.getValue();
         Integer salario = Integer.valueOf(txtFieldSalario.getText());
         Trabajador trabajador = new Trabajador(nombre, puesto, salario);
+        trabajador.insertarTrabajador(trabajador);
         Alert alerta = new Alert(Alert.AlertType.INFORMATION);
         alerta.setTitle("HECHO");
         alerta.setHeaderText("Mensaje");
